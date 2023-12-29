d8d7ba41637b4096516bfb16424821034ff34d41
Liliya-ivanovna <lilyasviatenko@ukr.net>
2023-12-29 11:23:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const token='6489917983:AAFLEIiiG1EXdXUSagWd3ZsF88g2vEtX3mI';
 
 const bot = new TelegramApi(token, {polling:true})
 
-bot.on('message', msg=>{
+bot.on('message',async msg=>{
     const text= msg.text;
     const chatId= msg.chat.id;
     if (text === '/start'){
-        bot.sendSticker(chatId, "")
-        bot.sendMessage(chatId, 'Привіт, як ти?')
+       await bot.sendSticker(chatId,"https://i.pinimg.com/564x/db/32/c3/db32c3b3912b53cd4ed7216a31540caa.jpg")
+        await bot.sendMessage(chatId, 'Привіт, як ти?')
     }
    
 })
